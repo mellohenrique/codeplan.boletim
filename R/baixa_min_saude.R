2b012c3a34e12fbb1c854d4cf182f8aeef70ceb2
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-08-18 13:35:20 -0300

    Adiciona arquivos iniciais do pacote

---
+++ R/baixa_min_saude.R
@@ -0,0 +1,18 @@
+#' @title Baixa dados do ministério da saúde
+#'
+#' @description Função que baixa os dados do ministério da saúde
+#'
+#' @return Um data.frame com os dados do ministério da saúde de COVID 19
+#'
+#' @examples
+#'
+
+baixa_min_saude <- function(){
+
+  resposta = httr::GET("https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral", httr::add_headers("X-Parse-Application-Id" = "unAFkcaNDeXajurGB7LChj8SgQYS2ptm"))
+
+  url = httr::content(resposta)[["results"]][[1]][["arquivo"]][["url"]]
+
+  utils::download.file(url, paste0("dados/", stringr::str_extract(url, "[0-9]+[a-z]+[0-9]+\\.[a-z]+$")), mode = 'wb')
+
+}
