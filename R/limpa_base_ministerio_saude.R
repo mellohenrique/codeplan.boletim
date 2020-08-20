7a30ed431c0d9f43fae01b5264ee6f6e85b67851
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-08-20 12:19:02 -0300

    Adiciona limpeza da base de dados do ministerio da saude por meio do pacote data.table

---
+++ R/limpa_base_ministerio_saude.R
@@ -0,0 +1,33 @@
+#' @title Limpa os dados do ministerio da saude para o formato utilizado pelo boletim
+#'
+#' @description Recebe os dados no formato utilizado pelo ministerio da saude e limpa eles para o formato usado pelo boletim da codeplan
+#'
+#' @param dados base dados carregada do ministerio da saude a ser lida
+#' @param produto_dt variavel logica que define se o produto da funcao e um data.table (se produto_dt == TRUE) ou um data.frame (se produto_dt = FALSE), padrao utilizado e FALSE
+#'
+#' @return Um data.frame com os dados do ministério da saúde de COVID 19
+#'
+#' @examples
+#'
+#'
+
+
+limpa_base_min_saude <- function(dados, produto_dt = FALSE){
+
+  dados <- data.table(dados)
+
+  dados <- dados[is.na(codmun) & regiao != "Brasil", .(data, estado, casosAcumulado, obitosAcumulado)]
+
+  setnames(dados, names(dados), c("date", "state", "confirmed", "deaths"))
+
+  dados[, date_tableau := as.character(format(as.Date(date),"%m-%d-%Y"))]
+
+  dados[, `:=`(dia_centesimo_caso = cumsum(confirmed >= 100),
+           dia_centesima_morte = cumsum(deaths >= 100)), by = state]
+
+  if(produto_dt) {
+    dados
+  } else {
+    as.data.frame(dados)
+  }
+}
