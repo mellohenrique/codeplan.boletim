6c19f597d0e667a1e6b6e5b04ca202caad5e4b28
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-09-15 18:46:20 -0300

    Adiciona base de dados para teste das funcoes do distrito federal

---
+++ R/limpa-dados-df.R
@@ -0,0 +1,67 @@
+#' @title Gera serie DF extracao direta
+#'
+#' @description Recebe os dados no formato utilizado pelo ministerio da saude e limpa eles para o formato usado pelo boletim da codeplan
+#'
+#' @inheritParams limpa_mortalidade_letalidade
+#' @param dados_df data.frame com os dados de cada caso do Distrito Federal
+#' @param ra Valor logico, se TRUE considera a serie historica por RA, se FALSE apenas para o Distrito Federal
+#'
+#' @return Um data.frame com os dados da serie historica do Distrito Federal
+#'
+#' @import data.table
+#'
+#' @export
+#'
+
+limpa_dados_df <- function(dados_df, ra = FALSE, produto_dt = FALSE){
+
+  dados_df <- data.table(dados_df)
+
+  dados_df[, `:=` (primeiros_sintomas = {as.Date(primeiros_sintomas)},
+                   dataObito = {as.Date(ifelse(estado_de_saude == "obito", as.character(dataobito), NA_character_))})]
+
+  if(ra){
+    casos = setDT(df)[, .N, keyby = c("primeiros_sintomas", "ra_localid")]
+    setnames(casos, c("data", "ra", "casos"))
+
+    obitos = setDT(df)[, .N, keyby = c("dataobito", "ra_localid")][!is.na(dataobito),]
+    setnames(obitos, c("data", "ra", "obitos"))
+
+    ra_data = expand.grid(seq(min(c(casos$data, obitos$data)), max(c(casos$data, obitos$data)), by="days"),
+                          unique(df$ra_localid))
+
+    ra_data = as.data.table(ra_data)
+    setnames(ra_data, c("data", "ra"))
+
+    df_serie = merge(ra_data, casos, all = TRUE, by = c("data" = "data", "ra" = "ra"))
+    df_serie = merge(df_serie, obitos, all = TRUE, by = c("data" = "data", "ra" = "ra"))
+
+    df_serie[, `:=`(casos = {ifelse(is.na(casos), 0, casos)},
+                    obitos = {ifelse(is.na(obitos), 0, obitos)},
+                    casos_acumulados = cumsum(casos),
+                    obitos_acumulados = cumsum(obitos)),
+             by = ra]
+
+  } else {
+    casos = setDT(df)[, .N, keyby = primeiros_sintomas]
+    setnames(casos, c("data", "casos"))
+
+    obitos = setDT(df)[, .N, keyby = dataobito][!is.na(dataobito),]
+    setnames(obitos, c("data", "obitos"))
+
+    datas = data.table(data = seq(min(c(casos$data, obitos$data)), max(c(casos$data, obitos$data)), by="days"))
+
+    df_serie = merge(datas, casos, all = TRUE, by = c("data" = "data"))
+    df_serie = merge(df_serie, obitos, all = TRUE, by = c("data" = "data"))
+
+    df_serie = merge(ra_data, casos, all = TRUE, by = c("data" = "data", "ra" = "ra"))
+    df_serie = merge(df_serie, obitos, all = TRUE, by = c("data" = "data", "ra" = "ra"))
+
+    df_serie[, `:=`(casos = {ifelse(is.na(casos), 0, casos)},
+                    obitos = {ifelse(is.na(obitos), 0, obitos)},
+                    casos_acumulados = cumsum(casos),
+                    obitos_acumulados = cumsum(obitos))]
+  }
+
+  df_serie
+}
---
+++ R/teste-df.R
@@ -0,0 +1,13 @@
+#' Base de teste para o pacote
+#'
+#' Uma base de dados ficticia para testar as funcoes do pacote do Distrito Federal
+#'
+#' @format Uma tabela com 17 colunas e 1300 linhas.
+#' \describe{
+#'   \item{id}{Variavel numerica com o id de cada observacao}
+#'   \item{primeiros_sintomas}{Variavel em data com a data dos primeiros sintomas}
+#'   \item{dataobito}{Variavel em data com a data do obito}
+#'   \item{estado_de_saude}{Variavel em caractere com o estado de saude da pessoa}
+#' }
+#' @source Tabela Ficticia para testar as funcoes para gerar dados da serie do Distrito Federal
+"teste_df"
