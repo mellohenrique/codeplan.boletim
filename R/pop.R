1babd45f40f0e25fa362c8481a91c1d4c9e7d60a
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-12-10 10:05:43 -0300

    Adiciona dados de populacao por RA

--- R/pop.R
+++ R/pop.R
@@ -2,9 +2,9 @@
 #'
 #' Uma base de dados da populacao por estados
 #'
-#' @format Uma tabela com 17 colunas e 1300 linhas.
+#' @format Uma tabela com 2 colunas e 35 linhas.
 #' \describe{
 #'   \item{estado}{Variavel em caractere com o estado da localidade}
 #'   \item{pop}{Variavel numerica com o numero de pessoas na localidade segundo o TCU para o ano de 2019}}
 #' }
-#' @source Tabela Criada a partir dos dadso do TCU
+#' @source Tabela Criada a partir dos dados do TCU
---
+++ R/pop_ra.R
@@ -0,0 +1,10 @@
+#' Base com populacao por regiao administrativa
+#'
+#' Uma base de dados da populacao por regiao administrativa do Distrito Federal
+#'
+#' @format Uma tabela com 2 colunas e 35 linhas.
+#' \describe{
+#'   \item{estado}{Variavel em caractere com o regiao administrativa da localidade}
+#'   \item{pop}{Variavel numerica com o numero de pessoas na regiao administrativa}}
+#' }
+#' @source Tabela Criada a partir dos dados da CODEPLAN
