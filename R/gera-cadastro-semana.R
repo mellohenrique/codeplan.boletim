5a097f7f599d38e16966bd2790306e5b19e5d586
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-12-10 10:06:23 -0300

    Adiciona funcao de gerar dados de cadastro por semana

--- R/gera-cadastro-semana.R
+++ R/gera-cadastro-semana.R
@@ -13,16 +13,23 @@
 
 gera_cadastro_semana <- function(dados_cadastro, data_inicio = "2012-01-01", produto_dt = FALSE){
   if(is.data.table(dados_cadastro)){
-    dados = dados[]
+    dados = dados
   } else {
     dados = data.table(dados_cadastro)
   }
 
-  data = data.table(data = seq(as.Date("2020-01-01"), max(dados$data), by = "day"))
+  data = data.table(data = seq(as.Date(data_inicio), max(dados$data), by = "day"))
 
   data[, epiweek := cumsum(weekdays(data) == "domingo") + 1]
 
   dados[data, on = .(data = data), epiweek := epiweek ]
+
+  dados[, .(casos = sum(casos)), by = .(epiweek, localidade)]
+
+  dados[as.data.table(pop_ra), on = .(localidade = ra), pop := pop]
+
+  dados[, `:=`(casos_100k = 100000 * casos / pop,
+               obitos_100k = 100000 * casos / pop)]
 
   retorna_dt(dados, produto_dt)
 }
