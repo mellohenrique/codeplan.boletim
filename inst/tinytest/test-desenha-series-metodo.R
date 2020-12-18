226d34fea7d36cd55e39ed3feac3cfd663c0d9af
Henrique Assuncao <henrique.mello1992@gmail.com>
2020-12-18 15:37:25 -0300

    Renomeia arquivos com nomes errados nos testes

--- inst/tinytest/test-desenha-series-metodo.R
+++ inst/tinytest/test-desenha-series-metodo.R
@@ -5,3 +5,4 @@ resultado <- desenha_series_metodo(limpa_min_saude(teste), teste_df)
 expect_equal(class(resultado), "list")
 expect_equal(sapply(resultado, class)[, 1], c("gg", "ggplot"))
 expect_equal(sapply(resultado, class)[, 2], c("gg", "ggplot"))
+
